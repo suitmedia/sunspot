6880f4acee15b8c7537d5a58213a85743dd44c43
Isaac Kearse <isaac@kearse.co.nz>
2010-06-03 13:54:37 +1200

    remove obselete code and fix boost

--- sunspot/lib/sunspot/rich_document.rb
+++ sunspot/lib/sunspot/rich_document.rb
@@ -13,29 +13,23 @@ module Sunspot
 
     def add(connection)
       params = {
-        :wt => :ruby,
-        'idx.attr' => false, # don't index any attributes, unless explicitly mapped
-        'map.title' => 'title_text',
+        :wt => :ruby
       }
 
       data = nil
 
-      @fields.each do |f|
-        puts f.name.to_s + " " + f.value.to_s
-        
+      @fields.each do |f|   
         if f.name.to_s.include?("_attachment")
           data = open(f.value).read
-          params['def.fl'] = f.name, # all text extracted goes to text_t (since it is a stored field, for highlighting)
           params['fmap.content'] = f.name
         else
           param_name = "literal.#{f.name.to_s}"
           params[param_name] = [] unless params.has_key?(param_name)
           params[param_name] << f.value          
         end
-
-#          if f.boost
-#            params["boost.#{f.name.to_s}"] = f.boost
-#          end
+        if f.attrs[:boost]
+          params["boost.#{f.name.to_s}"] = f.attrs[:boost]
+        end
       end
 
       solr_message = params
